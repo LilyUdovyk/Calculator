0b79da47dd4dfd7dc2495a662ac64d0d254c54a3
Lily Udovyk <pelinknew@gmail.com>
2020-07-29 00:01:11 +0300

    Updates

---
+++ precache-manifest.eab2b4f95f9ae278c8c5c32fc3b49f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb38dfd1b258b4d4d1750b124f4c01fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0134c4d2df965f24ee9",
+    "url": "./static/css/main.afc075b3.chunk.css"
+  },
+  {
+    "revision": "f917c2104598feef0d45",
+    "url": "./static/js/2.083eccd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.083eccd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0134c4d2df965f24ee9",
+    "url": "./static/js/main.176a99c9.chunk.js"
+  },
+  {
+    "revision": "5b155b1e54ab7e2dd0f2",
+    "url": "./static/js/runtime-main.dbea8c01.js"
+  }
+]);
